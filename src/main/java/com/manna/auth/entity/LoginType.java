1068bb8991c676f5ebb278f0ad41250d0ef9d845
nimusee1 <l08c0739@gmail.com>
2026-08-06 14:25:40 +0900

    fix: LoginType enum 개선

--- src/main/java/com/manna/auth/entity/LoginType.java
+++ src/main/java/com/manna/auth/entity/LoginType.java
@@ -1,17 +1,6 @@
 package com.manna.auth.entity;
 
 public enum LoginType {
-    GOOGLE("google"),
-    KAKAO("kakao");
-
-    private final String loginType;
-
-
-    LoginType(String loginType) {
-        this.loginType = loginType;
-    }
-
-    public String getLoginType() {
-        return loginType;
-    }
+    GOOGLE,
+    KAKAO
 }
