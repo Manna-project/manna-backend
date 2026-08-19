2a3ef8d9c424016d754254930e5e03f66f9300c8
nimusee1 <l08c0739@gmail.com>
2026-08-19 14:26:04 +0900

    chore: application-oauth2.yml 설정 추가 및 ClientProperties 불러오는 방식 개선

--- src/main/java/com/manna/auth/properties/OAuth2ClientProperties.java
+++ src/main/java/com/manna/auth/properties/OAuth2ClientProperties.java
@@ -3,9 +3,20 @@ package com.manna.auth.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
+import java.util.Map;
 
-@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration")
-public record OAuth2RegistrationProperties(RegistrationConfig kakao) {
+@ConfigurationProperties(prefix = "spring.security.oauth2.client")
+public record OAuth2ClientProperties(
+        Map<String, ProviderConfig> provider,
+        Map<String, RegistrationConfig> registration
+) {
+    public record ProviderConfig(
+            String authorizationUri,
+            String tokenUri,
+            String userInfoUri,
+            String userNameAttribute
+    ) {}
+
     public record RegistrationConfig(
             String clientId,
             String clientSecret,
